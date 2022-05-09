1cb8a17341b220974104f79bdee59b816fb976dc
PuihKhao25 <pkhao1999@gmail.com>
2022-05-09 14:56:59 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('hello')
 })
 
 const port = 3000
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Listening ${port}`)
 })
